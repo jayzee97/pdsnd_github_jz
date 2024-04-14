8ce79ab650b6ccf006cf858156c0b89e32ef3799
Jennifer Zewatsky <jennifer.zewatsky@gmail.com>
2024-04-13 20:35:03 -0400

    Initial commit of Python code and list of files to ignore

---
+++ ExploreData.py
@@ -0,0 +1,36 @@
+# Importing modules 
+import pandas as pd
+import numpy as np 
+import functools 
+
+# Reading in public-use source data, downloaded from Census Bureau
+allUnits = pd.read_csv(r'/Users/Jenny/udacity-git-course/pdsnd_github_jz/allunits_puf_21.csv')
+occUnits = pd.read_csv(r'/Users/Jenny/udacity-git-course/pdsnd_github_jz/occupied_puf_21.csv')
+
+# Dropping unit weight fields for estimating variance, as these will not be used
+allUnits2 = allUnits.drop(allUnits.iloc[:, 25:105], axis=1)
+occUnits2 = occUnits.drop(occUnits.iloc[:, 152:232], axis=1)
+
+# Merging together the parents (allunits) table with the occupied units table to calculate statistics 
+final = pd.merge(allUnits2, occUnits2, how='left', on='CONTROL')
+
+occupancy_stats = final.query("OCC == 1").groupby('BORO')['OCC'].count().reset_index(name='occupied_units')
+
+# Determining counts, by NYC borough, of reported instances of various problems known to occur in housing (value of 1 indicates presence of problem)
+floor_stats = final.query("FLOORHOLES == 1").groupby('BORO')["FLOORHOLES"].count().reset_index(name='has_floorholes') 
+wall_stats = final.query("WALLHOLES == 1").groupby('BORO')["WALLHOLES"].count().reset_index(name='has_wallholes')
+leak_stats = final.query("LEAKS == 1").groupby('BORO')["LEAKS"].count().reset_index(name='has_leaks')
+mold_stats = final.query("MOLD == 1").groupby('BORO')["MOLD"].count().reset_index(name='has_mold')
+
+# Merging all summary tables together
+frames = [occupancy_stats, floor_stats, wall_stats, leak_stats, mold_stats]
+borough_issues = functools.reduce(lambda left, right: pd.merge(left, right, on='BORO'), frames)
+borough_issues['pct_floorholes'] = borough_issues['has_floorholes'] / borough_issues['occupied_units']
+borough_issues['pct_wallholes'] = borough_issues['has_wallholes'] / borough_issues['occupied_units']
+borough_issues['pct_leaks'] = borough_issues['has_leaks'] / borough_issues['occupied_units']
+borough_issues['pct_mold'] = borough_issues['has_mold'] / borough_issues['occupied_units']
+
+# looking at results
+borough_issues.head(5)
+
+
