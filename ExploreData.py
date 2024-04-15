17c2e08c6649406eaaae2c81236bb41e4df38f2c
Jennifer Zewatsky <jennifer.zewatsky@gmail.com>
2024-04-14 22:24:09 -0400

    Modified comments in code to improve clarity

--- ExploreData.py
+++ ExploreData.py
@@ -1,13 +1,13 @@
-# Importing modules 
+# Importing modules required to explore survey data
 import pandas as pd
 import numpy as np 
 import functools 
 
-# Reading in public-use source data, downloaded from Census Bureau
+# Reading in public-use source data, downloaded from Census Bureau and saved in '~/udacity-git-course/pdsnd_github_jz'
 allUnits = pd.read_csv(r'/Users/Jenny/udacity-git-course/pdsnd_github_jz/allunits_puf_21.csv')
 occUnits = pd.read_csv(r'/Users/Jenny/udacity-git-course/pdsnd_github_jz/occupied_puf_21.csv')
 
-# Dropping unit weight fields for estimating variance, as these will not be used
+# Dropping unit weight fields for estimating variance to enhance readability of tables, as these fields will not be used
 allUnits2 = allUnits.drop(allUnits.iloc[:, 25:105], axis=1)
 occUnits2 = occUnits.drop(occUnits.iloc[:, 152:232], axis=1)
 
@@ -22,7 +22,7 @@ wall_stats = final.query("WALLHOLES == 1").groupby('BORO')["WALLHOLES"].count().
 leak_stats = final.query("LEAKS == 1").groupby('BORO')["LEAKS"].count().reset_index(name='has_leaks')
 mold_stats = final.query("MOLD == 1").groupby('BORO')["MOLD"].count().reset_index(name='has_mold')
 
-# Merging all summary tables together
+# Merging all summary tables together to create combined, borough-level, table
 frames = [occupancy_stats, floor_stats, wall_stats, leak_stats, mold_stats]
 borough_issues = functools.reduce(lambda left, right: pd.merge(left, right, on='BORO'), frames)
 borough_issues['pct_floorholes'] = borough_issues['has_floorholes'] / borough_issues['occupied_units']
@@ -30,7 +30,7 @@ borough_issues['pct_wallholes'] = borough_issues['has_wallholes'] / borough_issu
 borough_issues['pct_leaks'] = borough_issues['has_leaks'] / borough_issues['occupied_units']
 borough_issues['pct_mold'] = borough_issues['has_mold'] / borough_issues['occupied_units']
 
-# looking at results
+# Viewing results
 borough_issues.head(5)
 
 
